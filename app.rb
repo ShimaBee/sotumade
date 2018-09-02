ac20ffd21cf5352fbbbee08e2f10b847c5d6e293
Yuta1122 <mrs1122ani@gmail.com>
2018-09-02 14:03:22 +0900

    rename input-name

--- app.rb
+++ app.rb
@@ -14,9 +14,18 @@ get '/input' do
 end
 
 post '/result' do
-  @common = params[:common].to_i
-  @compulsory = params[:compulsory].to_i
-  @expertSelect = params[:expertSelect].to_i
+  @exercis = params[:exercise].to_i
+  @humanity = params[:humanity].to_i
+  @society = params[:society].to_i
+  @synthesis = params[:synthesis].to_i
+  @feature = params[:future].to_i
+  @information = params[:infomation].to_i
+  @language1 = params[:language1].to_i
+  @language2 = params[:language2].to_i
+  @dep_feature = params[:DFuture].to_i
+  @dep_basic_compulsory = params[:DBasicCompulsory].to_i
+  @dep_basic_select = params[:DBasicSelect].to_i
+  @dep_select = params[:DSelect].to_i
   @freedom = params[:freedom].to_i
 
   if @common >= 40  && @compulsory >= 34 && @expertSelect >= 26 && @freedom >= 24
